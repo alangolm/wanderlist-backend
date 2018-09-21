28520d30389ecf6ae15805352b48da8d3a64ed55
github username <github email address>
2018-09-21 11:41:53 -0400

    added several gems for auth and figaro

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,6 +23,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :destinations
   has_many :events, through: :destinations
-  validates :username, {presence: true, uniqueness: true}
+  validates :username, {presence: true, uniqueness: true, case_sensitive: false}
 end
